f3f245eb01c4b7a6ccdc70688a42fa2026f0840d
Lucian Green <luciangreen@lucianacademy.com>
2024-02-20 11:21:12 +1100

    Update luciancicd_ws1.pl

--- luciancicd_ws1.pl
+++ luciancicd_ws1.pl
@@ -51,8 +51,8 @@ foldr(string_concat,["chmod +x ",GP,"\n","swipl -g main -q ./",GP],S3),%,
 ((catch(bash_command(S3,HTML), _, (foldr(string_concat,["Warning."%%"Error: Can't clone ",User3,"/",Repository3," repository on GitHub."
 	],_Text4),%writeln1(Text4),
 	fail%abort
- 	)),delete_tmp)%sd2(R110,Tests1,RTests,R1,To_m_1,Repository_root_path,Repository,Gitl_data_path1,N,R1,N_path,To,HTML)
- ->HTML1=HTML;
+ 	)),delete_tmp,term_to_atom(HTML2,HTML))%sd2(R110,Tests1,RTests,R1,To_m_1,Repository_root_path,Repository,Gitl_data_path1,N,R1,N_path,To,HTML)
+ ->HTML1=HTML2;
  HTML1="Identical"),
 
  working_directory1(_,A),
