f0a4785547946622e9c4d5d6ceff6f6afe29fad6
Lucian Green <greenlucian1@gmail.com>
2025-10-20 05:47:27 +1100

    Update luciancicdverify.pl

--- luciancicdverify.pl
+++ luciancicdverify.pl
@@ -139,7 +139,7 @@ lc_test0(8,true,7,i,
 ).
 %*
 
-% Finds A=2,B=1 from A=2,B=2 and A=1,B=1 (test 8).
+% Finds A=1,B=2 from A=2,B=2 and A=1,B=1 (test 8).
 
 lc_test0(9,false,16,d,
 [["c/c.pl","%c(A).\n%A=3.\nc(C):-A=2,B=2,C is A+B."],
